8ed59fecc0bdc6ee5fe81dcb2fa1369eea21b6d3
GASHAMURA <gasharsene@gmail.com>
2023-09-15 09:04:09 -0700

    function check uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _isupper - checks for uppercase character.
+ * @c: charactor to be checked
+ * Return: 1 if c is uppercase,0 otherwise
+ */
+int _isupper(int c)
+{
+		if (isupper(c))
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+}
---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - fuction that prints _putchar
+ * Return:return o;
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
