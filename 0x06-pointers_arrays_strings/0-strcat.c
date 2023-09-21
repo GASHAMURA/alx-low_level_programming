01204a063e5b57db427fc1cd87533a97a1dce099
GASHAMURA <gasharsene@gmail.com>
2023-09-21 14:13:40 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,18 +6,18 @@
  *
  * Return: void
  */
-char *_strcat(char *dest,char *src)
+char *_strcat(char *dest, char *src)
 {
 	int i;
 	int j;
 
-	i=0;
-	while (dest[i] !='\0')
+	i = 0;
+	while (dest[i] != '\0')
 	{
 		i++;
 	}
-	j=0;
-	while (src[j] !='\0')
+	j = 0;
+	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
 		i++;
