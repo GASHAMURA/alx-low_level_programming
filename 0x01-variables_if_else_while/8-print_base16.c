310ea46731590e934e9a7f37bb98a4a00b84d5da
GASHAMURA <gasharsene@gmail.com>
2023-09-25 07:31:09 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Print all possible combinations of single-digit numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	for (i = 97; i < 103; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
