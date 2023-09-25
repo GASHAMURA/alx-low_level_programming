a5dee11cd4eb4be527779f80e404f798af5ca665
GASHAMURA <gasharsene@gmail.com>
2023-09-25 02:53:52 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,13 +7,11 @@ void print_alphabet_x10(void)
 
 	char alpha;
 	int i;
-	
+
 	for (i = 0; i < 10; i++)
 	{
-	
 		for (alpha = 'a'; alpha <= 'z'; alpha++)
 		{
-		
 			_putchar(alpha);
 		}
 		_putchar('\n');
