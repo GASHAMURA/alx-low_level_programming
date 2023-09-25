a1d1b376d863a2e1f53cdfbf75f78bcce6f12972
GASHAMURA <gasharsene@gmail.com>
2023-09-25 07:23:52 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 /**
- * main - Print the alphabet
+ * main - Print the alphabet without q and e
  * Return: 0
  */
 int main(void)
 {
-	char alpha[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
 
-	for (i = 0; i < 52; i++)
+	for (i = 97; i < 123; i++)
 	{
-		putchar(alpha[i]);
+		if (i != 101 && i != 113)
+		{
+			putchar(i);
+		}
 	}
 	putchar('\n');
 	return (0);
