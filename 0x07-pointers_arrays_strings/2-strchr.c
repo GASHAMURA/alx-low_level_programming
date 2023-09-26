50f9fbb58c424e5bfc93835a19c1f52ca0690219
GASHAMURA <gasharsene@gmail.com>
2023-09-26 14:51:42 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strchr - locate character in string
+ * @s: input
+ * @c: input
+ *
+ * Return: 0
+ */
+
+char *_strchr(char *s, char c)
+{
+	return (strchr(s, c));
+}
