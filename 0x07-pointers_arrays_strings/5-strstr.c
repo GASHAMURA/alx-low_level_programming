3ea8b988e5610762ffc91e38f6b4957b21750790
GASHAMURA <gasharsene@gmail.com>
2023-09-26 15:02:13 -0700

    5-main.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strstr - locate a substring
+ * @haystack: input
+ * @needle: input
+ *
+ * Return: 0
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	return (strstr(haystack, needle));
+}
