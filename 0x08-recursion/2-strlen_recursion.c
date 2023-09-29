d7bc0b1b4da8e415fd9f93a1c0d7b8de255b89bf
GASHAMURA <gasharsene@gmail.com>
2023-09-29 03:14:20 -0700

    2-strlen_recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen_recursion - returns the length of a string.
+ * @s: string length to be calculated
+ * Return: length of the string
+ */
+int _strlen_recursion(char *s)
+{
+	return (strlen(s));
+}
