88c41bc4741f40b8b250cbe7916eb02e86e901de
GASHAMURA <gasharsene@gmail.com>
2023-09-30 02:15:01 -0700

    0-isupper.c

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _isupper - checks for uppercase character
+ *
+ * @c: Char to be checked
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+int _isupper(int c)
+{
+	if (isupper(c))
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _memset - fill memory with a constant byte.
+ * @s: starting address of memory to be filled
+ * @b: the desired value
+ * @n: number of bytes to be changed
+ * Return: a pointer to the memory area s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	return (memset(s, b, n));
+}
