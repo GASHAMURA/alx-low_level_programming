f774b7e432ac3bfbf977e5c200a220b8017fbc21
GASHAMURA <gasharsene@gmail.com>
2023-10-21 05:22:14 -0700

    my new collection

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - Computes the absolute value of an integer
+ * @n: the integer check
+ * Return: absolute value of int
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
